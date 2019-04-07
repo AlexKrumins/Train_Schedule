d306fffc4937bf91764106be459a4427ebe08e32
Alex Krumins <justkrumins@gmail.com>
2019-04-06 22:17:51 -0500

    option menus entered for time selection

--- timesheetLogic.js
+++ timesheetLogic.js
@@ -1,12 +1,3 @@
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new Trains - then update the html + update the database
-// 3. Create a way to retrieve Trains from the Train database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyCGRzvTzk3D5wHcyo7i2CB7x99M4NfpSyE",
@@ -20,19 +11,25 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// 2. Button for adding Trains
+for (i=1;i<=12;i++){
+  $("#start-input-hour").append($('<option></option>').val(i).html(i))
+}
+for (i=00;i<=59;i++){
+  if (i<10) {
+    $("#start-input-minute").append($('<option></option>').val("0"+i).html("0"+i))
+  }else{
+  $("#start-input-minute").append($('<option></option>').val(i).html(i))
+  };
+}
+
 $("#add-train-btn").on("click", function(event) {
   event.preventDefault();
 
-  // Grabs user input
   var chchName = $("#train-name-input").val().trim();
   var chchDestination = $("#destination-input").val().trim();
-  var chchStart = ($("#start-input").val().trim(), 'HH:mm');
+  var chchStart = moment($("#start-input-hour").val() + ":" + $("#start-input-minute").val() + $("#start-input-ampm").val(), "hmm", false);
   console.log('train start time', chchStart);
   var chchFrequency = $("#frequency-input").val().trim();
-  console.log(chchStart);
-
-  // Creates local "temporary" object for holding Train data
   var newChch = {
     name: chchName,
     destination: chchDestination,
@@ -40,16 +37,9 @@ $("#add-train-btn").on("click", function(event) {
     frequency: chchFrequency
   };
 
-  // Uploads Train data to the database
   database.ref().push(newChch);
 
-  // Logs everything to console
-  console.log(newChch.name);
-  console.log(newChch.destination);
-  console.log(newChch.start);
-  console.log(newChch.frequency);
-
-  alert("Train successfully added");
+  alert("It looks like everything is on track");
 
   // Clears all of the text-boxes
   $("#train-name-input").val("");
@@ -58,52 +48,20 @@ $("#add-train-btn").on("click", function(event) {
   $("#frequency-input").val("");
 });
 
-// 3. Create Firebase event for adding Train to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot) {
   console.log(childSnapshot.val());
 
-  // Store everything into a variable.
   var chchName = childSnapshot.val().name;
   var chchDestination = childSnapshot.val().destination;
   var chchStart = childSnapshot.val().start;
   var chchFrequency = childSnapshot.val().frequency;
+  var now = moment();
 
-  // Train Info
-  console.log(chchName);
-  console.log(chchDestination);
-  console.log(chchStart);
-  console.log(chchFrequency);
-
-  // Prettify the Train start
-  var chchStartPretty = moment.unix(chchStart).format("MM/DD/YYYY");
-
-  // Calculate the months worked using hardcore math
-  // To calculate the months worked
-  var chchMonths = moment().diff(moment(chchStart, "X"), "months");
-  console.log(chchMonths);
-
-  // Calculate the total billed frequency
-  var chchBilled = chchMonths * chchFrequency;
-  console.log(chchBilled);
-
-  // Create the new row
   var newRow = $("<tr>").append(
     $("<td>").text(chchName),
     $("<td>").text(chchDestination),
     $("<td>").text(chchFrequency),
-    $("<td>").text(chchStartPretty),
-    $("<td>").text(chchMonths),
-    $("<td>").text(chchBilled)
   );
 
-  // Append the new row to the table
   $("#train-table > tbody").append(newRow);
 });
-
-// Example Time Math
-// -----------------------------------------------------------------------------
-// Assume Train start date of January 1, 2015
-// Assume current date is March 1, 2016
-
-// We know that this is 15 months.
-// Now we will create code in moment.js to confirm that any attempt we use meets this test case
